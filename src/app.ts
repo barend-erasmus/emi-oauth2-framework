83e1baa8f47f280201e25540c6a4e45c7d83047c
Barend Erasmus <developersworkspace@gmail.com>
2017-08-15 10:52:22 +0200

    update

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import * as express from 'express';
 import * as path from 'path';
 import * as yargs from 'yargs';
 import * as request from 'request-promise';
+import * as cors from 'cors';
 
 import * as ActiveDirectory from 'activedirectory';
 
@@ -20,12 +21,17 @@ app.set('trust proxy', true);
 // Configures middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use('/auth', OAuth2FrameworkRouter(
     {
         findClient: (clientId: string) => {
             if (clientId === '0zyrWYATtw') {
-                return Promise.resolve(new Client('Euromonitor International', '0zyrWYATtw', 'x3h8CTB2Cj', [], ['http://localhost:3000/auth/passport/callback', 'http://cpt.innovation.euromonitor.local/auth/passport/callback', 'http://localhost:4200/callback'], true));
+                return Promise.resolve(new Client('FDM - Parking App', '0zyrWYATtw', 'x3h8CTB2Cj', [], ['http://localhost:4200/callback', 'http://parkingapp.euromonitor.local/web/callback'], true));
+            } else if (clientId === '88MHne8ije') {
+                return Promise.resolve(new Client('Internal - Passport', '88MHne8ije', 'KI69NoJ0uO', [], ['http://localhost:3000/auth/passport/callback', 'http://cpt.innovation.euromonitor.local/auth/passport/callback', 'http://localhost:4200/callback', 'http://parkingapp.euromonitor.local/web/callback'], true));
+            } else if (clientId === 'FUmCsF9c2D') {
+                return Promise.resolve(new Client('Feature Toggle', 'FUmCsF9c2D', '4vmPj7eGVp', [], ['http://localhost:4200/callback', 'http://cpt.innovation.euromonitor.local/featuretoggle/callback'], true));
             } else {
                 return Promise.resolve(null);
             }
@@ -67,9 +73,9 @@ app.use('/auth', OAuth2FrameworkRouter(
 
 app.get('/auth/passport/callback', (req: express.Request, res: express.Response) => {
 
-    const client_id = '0zyrWYATtw';
-    const client_secret = 'x3h8CTB2Cj';
-    const redirect_uri = 'http://localhost:3000/auth/passport/callback';
+    const client_id = '88MHne8ije';
+    const client_secret = 'KI69NoJ0uO';
+    const redirect_uri = argv.prod ? 'http://cpt.innovation.euromonitor.local/auth/passport/callback' : 'http://localhost:3000/auth/passport/callback';
     const trinityApiUri = 'http://trinity.euromonitor.com';
 
     co(function* () {
@@ -82,7 +88,7 @@ app.get('/auth/passport/callback', (req: express.Request, res: express.Response)
                 grant_type: 'authorization_code'
             },
             method: 'POST',
-            uri: `http://localhost:3000/auth/token`,
+            uri: argv.prod ? `http://cpt.innovation.euromonitor.local/auth/token` : `http://localhost:3000/auth/token`,
             json: true,
             resolveWithFullResponse: true,
         });
@@ -96,7 +102,7 @@ app.get('/auth/passport/callback', (req: express.Request, res: express.Response)
 
         const response2 = yield request({
             method: 'GET',
-            uri: `http://localhost:3000/auth/user`,
+            uri: argv.prod ? `http://cpt.innovation.euromonitor.local/auth/user` : `http://localhost:3000/auth/user`,
             headers: {
                 Authorization: `Bearer ${response1.body.access_token}`,
             },
